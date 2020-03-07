954cd11416afef0648f246dbd89c540fb8d9c738
Xavier Perez <xperez007@gmail.com>
2020-03-07 13:21:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,8 +32,8 @@ end
 array_of_links.map do |link|
     binding.pry
  
- {:name =>"Arron Enser", :location => "Scotsdale, AZ":profile_url =>"students/aaron-enser.html",
- {:name =>"Jenny Yamada", :location => ""
+ {:name =>"Arron Enser", :location => "Scotsdale, AZ",:profile_url =>"students/aaron-enser.html",
+ {:name =>"Jenny Yamada", :location => "Portland, OR", :profile_url => 
 Scraper.scrape_index_page(index_url)
 Scraper.scrape_profile_page(profile_url)
 Self.all = self 
