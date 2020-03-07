be8a64d794b9c551ac16c4c0e4e66ec7123b7213
Xavier Perez <xperez007@gmail.com>
2020-03-07 13:14:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,14 +32,7 @@ end
 array_of_links.map do |link|
     binding.pry
  
- {:name=>"Bacon McRib", :location=>"Kansas City, MO"}
- {:name=>"Alisha McWilliams", :location=>"San Francisco, CA"}
- {:name=>"Daniel Fenjves", :location=>"Austin, TX"}
- {:name=>"Arielle Sullivan", :location=>"Chicago, IL"}
- {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"}
- {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"}]}
-
-
+ 
 Scraper.scrape_index_page(index_url)
 Scraper.scrape_profile_page(profile_url)
 Self.all = self 
