e5e05f8680e367f116679a650f2684d93ff0cbde
Xavier Perez <xperez007@gmail.com>
2020-03-07 13:12:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,49 @@
+require environment nokogiri
+require environment open-url 
+require environment bundler
+require environment pry
 require 'open-uri'
 require 'pry'
 
 class Scraper
+  attr_accessor:name, :location, :index_url
+    @@all = []
+    
+  end
+end
+
 
   def self.scrape_index_page(index_url)
+    scrape_index_page = scrape_index_page
+    @@all << self 
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+      self.scrape_profile_page = self.scrape_profile_page
+      @@all << self
+  end
+  
   end
 
 end
 
+
+array_of_links.map do |link|
+    binding.pry
+ 
+ {:name=>"Bacon McRib", :location=>"Kansas City, MO"}
+ {:name=>"Alisha McWilliams", :location=>"San Francisco, CA"}
+ {:name=>"Daniel Fenjves", :location=>"Austin, TX"}
+ {:name=>"Arielle Sullivan", :location=>"Chicago, IL"}
+ {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"}
+ {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"}]}
+
+
+Scraper.scrape_index_page(index_url)
+Scraper.scrape_profile_page(profile_url)
+Self.all = self 
+
+
+  end
+end
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
+require environment nokogiri
+require environment open-url 
+require environment bundler
+require environment pry
+require 'open-uri'
+require 'pry
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -20,4 +27,6 @@ class Student
     
   end
 end
+
+
 
