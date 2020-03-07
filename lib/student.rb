ea8dcaf1d2bc0fe50ce055b91a3298b25a23e0da
Xavier Perez <xperez007@gmail.com>
2020-03-07 13:36:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ require 'open-uri'
 require 'pry
 
 class Student
-  let!(:student_index_array) {[{:name=>"Alex Patriquin", :location=>"New York, NY"},
+  let!(:student_index_array) {[{:name=>"Arron Enser", :location=>"},
  {:name=>"Bacon McRib", :location=>"Kansas City, MO"},
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
